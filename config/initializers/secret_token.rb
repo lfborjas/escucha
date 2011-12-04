1c9c873a2a0b6cb1787b593c9b0eaeba773766d9
lfborjas <luis.borjas@escolarea.com>
2011-12-04 04:13:00 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Escucha::Application.config.secret_token = 'cfa5730987efa1acadcac7acdeb9d386549054781d999cafe7958720f2521089ad025e49a2dee818f41fca2a2ff950015a8ee4d4311d9986c34ecca1e076706e'
